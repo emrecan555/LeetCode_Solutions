bca6a160628f963bceb6d98ef5b15e1a66701e98
Emrecan <73848894+emrecan555@users.noreply.github.com>
2025-10-29 18:58:13 +0300

    Update 3sum.cpp

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,7 +6,7 @@ public:
         sort(nums.begin(), nums.end());  // Önce sırala
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Aynı sayıyı tekrar kullanma
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.size() - 1;
@@ -18,7 +18,6 @@ public:
                     left++;
                     right--;
 
-                    // Aynı değerleri atla
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
                 } else if (sum < 0) {
